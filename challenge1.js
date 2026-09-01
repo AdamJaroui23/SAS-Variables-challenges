3815c0e03933c9926598bf85c8cff6061c3a0351
Adam Jaroui <adamjaroui23@gmail.com>
2026-09-01 10:27:43 +0200

    challenge 1

---
+++ challenge1.js
@@ -0,0 +1,10 @@
+let Pseudo = "Adam";
+let Niveau = 1;
+let Age = 19;
+let Score = 2000;
+let Pays = "Morocco";
+console.log("Pseudo : ", Pseudo);
+console.log("Niveau : " , Niveau);
+console.log("Age : " , Age)
+console.log("Score : " , Score)
+console.log("Pays : " , Pays)
